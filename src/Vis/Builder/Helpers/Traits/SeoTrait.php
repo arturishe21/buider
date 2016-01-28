2b45b49b9f34e2b98cec14e069b62418559aed21
arturishe <artur@vis-design.com>
2016-01-28 16:47:14 +0100

    fix multilang

--- src/Vis/Builder/Helpers/Traits/SeoTrait.php
+++ src/Vis/Builder/Helpers/Traits/SeoTrait.php
@@ -7,9 +7,9 @@ trait SeoTrait
     public function getSeoTitle()
     {
         if( isset($this->seo_title) && !empty($this->seo_title)) {
-            return $this->seo_title;
+            return $this->t('seo_title');
         } elseif(isset($this->title)) {
-            return $this->title;
+            return $this->t('title');
         }
 
     } // end getSeoTitle
@@ -17,9 +17,9 @@ trait SeoTrait
     public function getSeoDescription()
     {
         if( isset($this->seo_description) && !empty($this->seo_description)) {
-            return $this->seo_description;
+            return $this->t('seo_description');
         } elseif(isset($this->short_description)) {
-            return $this->short_description;
+            return $this->t('short_description');
         }
 
     } // end getSeoTitle
